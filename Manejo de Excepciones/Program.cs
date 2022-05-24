066abd5fe966c1c8062c655f539b4ad717de827e
GanzosRozas <Javi282015@gmail.com>
2022-05-24 11:24:27 -0700

    Excepciones numeros elevados

---
+++ Manejo de Excepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejo_de_Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Escribe un numero para elevarlo al cuadrado ");
+            try
+            {
+                numero = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(numero * numero);
+            }
+            catch (FormatException )
+            {
+
+                Console.WriteLine("El tipo de dato no es entero pruebe con un que no tenga punto decimal");
+                Console.WriteLine("Presione enter para salir");
+                Console.ReadKey();
+            }
+            catch(OverflowException )
+            {
+                Console.WriteLine("El numero es demasiado grande ");
+                Console.WriteLine("Presione enter para salir");
+                Console.ReadKey();
+            }
+
+
+            
+        }
+    }
+}
